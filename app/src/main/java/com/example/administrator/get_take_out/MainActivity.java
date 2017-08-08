cf9aa7d41e5ca5477fab03d0f53716e0a5e55f48
xiaguojin <life_for_summer@foxmail.com>
2017-08-08 14:40:39 +0800

    first commit

---
+++ app/src/main/java/com/example/administrator/get_take_out/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.get_take_out;
+
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.print("haha");
+    }
+}
